02d9056d23b8a6c6dfbaab2971331e06ce6135a2
bwood3 <bryanwood1207@gmail.com>
2023-04-23 22:55:35 -0400

    About update

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,9 @@ const About = () => {
             <h1>About</h1>
             <p>Bryan Wood</p>
             <p>
-                Order service front end app. Ensure that orderservice backend is running. Customer Id of 1 currently has data (Note: data may take time to load. Try refreshing if it takes to long).
+                Order service front end app.
             </p>
+            <p>Customer Id of 1 currently has data (Note: data may take time to load. Try refreshing if it takes too long).</p>
         </div>
     );
 };
